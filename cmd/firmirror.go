0e55895450da713f04098106e8e921f24cf869a1
Jonathan Amiez <j.amiez@criteo.com>
2026-01-23 21:49:27 +0100

    Move cli options to main package
    It has no use in firmirror package
    
    Change-Id: I4d6b748ce1ae36948481b727eb6775493c22b0fb

--- cmd/firmirror.go
+++ cmd/firmirror.go
@@ -14,18 +14,41 @@ import (
 	"github.com/criteo/firmirror/pkg/vendors/hpe"
 )
 
+type DellFlags struct {
+	Enable     bool     `help:"Enable Dell firmware fetching." default:"false"`
+	MachinesID []string `help:"List of machine IDs to fetch firmware for. They are composed of 4 characters representing the machine type, followed by 4 digits representing the hexadecimal machine ID. For example: \"0C60\" for \"3168\" corresponding to the C6615 series of servers. You can also specify \"*\" to fetch all the firmware, but this may take a very long time."`
+}
+
+type HPEFlags struct {
+	Enable bool     `help:"Enable HPE firmware fetching." default:"false"`
+	Gens   []string `help:"List of generations to fetch firmware for." default:"gen10,gen11,gen12" enum:"gen10,gen11,gen12"`
+}
+
+var args struct {
+	DellFlags `embed:"" prefix:"dell." group:"Dell" help:"Dell firmware fetching."`
+	HPEFlags  `embed:"" prefix:"hpe." group:"HPE" help:"HPE firmware fetching."`
+	OutputDir string `help:"Output directory for the LVFS-compatible firmware repository (ignored when using S3)" type:"path"`
+	Refresh   struct {
+	} `cmd:"" help:"Refresh all the firmware from the repositories. Note: this will not replace the already-existing firmware, even if the vendor pushed an updated version. You will need to delete the firmware manually."`
+}
+
 func main() {
-	cli := kong.Parse(&firmirror.CLI)
+	cli := kong.Parse(&args)
 	switch cli.Command() {
-	case "refresh <out-dir>":
+	case "refresh":
 	default:
 		panic(cli.Command())
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 
+	if args.OutputDir == "" {
+		slog.Error("Output directory is required when using local storage")
+		return
+	}
+
 	// Create storage backend (default to local filesystem)
-	storage, err := firmirror.NewLocalStorage(firmirror.CLI.Refresh.OutDir)
+	storage, err := firmirror.NewLocalStorage(args.OutputDir)
 	if err != nil {
 		slog.Error("Failed to create storage backend", "error", err)
 		return
@@ -35,23 +58,23 @@ func main() {
 		CacheDir: ".firmirror_cache",
 	}
 
-	if !firmirror.CLI.HPEFlags.Enable && !firmirror.CLI.DellFlags.Enable {
+	if !args.HPEFlags.Enable && !args.DellFlags.Enable {
 		slog.Error("No vendor enabled, exiting")
 		return
 	}
 
 	fm := firmirror.NewFirmirrorSyncer(config, storage)
 
-	if firmirror.CLI.HPEFlags.Enable {
-		for _, gen := range firmirror.CLI.HPEFlags.Gens {
+	if args.HPEFlags.Enable {
+		for _, gen := range args.HPEFlags.Gens {
 			hpeRepo := "fwpp-" + gen
 			hpeVendor := hpe.NewHPEVendor(hpeRepo)
 			fm.RegisterVendor("hpe-"+gen, hpeVendor)
 		}
 	}
 
-	if firmirror.CLI.DellFlags.Enable {
-		dellVendor := dell.NewDellVendor(firmirror.CLI.DellFlags.MachinesID)
+	if args.DellFlags.Enable {
+		dellVendor := dell.NewDellVendor(args.DellFlags.MachinesID)
 		fm.RegisterVendor("dell", dellVendor)
 	}
 
